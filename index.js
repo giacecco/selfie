7ea70bfe0edb9554cc3cb9095ba71ba7e5ad88d5
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2013-12-11 13:21:14 +0000

    Added querystring support

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+var qs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+        var p=a[i].split('=');
+        if (p.length != 2) continue;
+        b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+})(window.location.search.substr(1).split('&'));
+
 var switchColour = function (colour) {
 	$("#frontImage").attr("src", colour ? "colour.png" : "desaturated.png");
 }
